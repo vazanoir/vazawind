0fae0a1c9af80f0a7ce84eaa0b0f5d7d40d17b1f
Vazanoir <vazanoir@outlook.fr>
2022-07-22 22:02:12 +0200

    Change eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
     'import/extensions': ['error', 'never'],
     'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
     'react/require-default-props': 0,
-    'no-unused-vars': ['warn', { vars: 'all', args: 'none', ignoreRestSiblings: false }],
-    'no-undef': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-unused-vars': 'off',
   },
 };
